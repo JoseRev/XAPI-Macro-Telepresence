ebd0ec357ba608f37954d027c13090fa953d8c90
JoseRev <43868903+JoseRev@users.noreply.github.com>
2021-08-31 12:11:50 -0500

    Add files via upload

---
+++ One Button SIP/Memory_Storage.js
@@ -0,0 +1,12 @@
+var memory = {
+    "key": "value",
+    "myGlobalVar": "[a-zA-Z]",
+    "myMacro": {
+        "VAR1": "TIFON"
+    },
+    "./Videoconference3rd_Pty": {
+        "URIs": [
+            "148393"
+        ]
+    }
+}
